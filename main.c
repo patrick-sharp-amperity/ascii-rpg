be9fc753eb09b6f17f3724a9cf9c4512dfa0fbee
patrick <psharp@greenspice.com>
2023-12-02 11:54:13 -0800

    video 04: functions for drawing rooms

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+typedef struct Room
+{
+  int xPosition;
+  int yPosition;
+  int height;
+  int width;
+  // Monster **monsters;
+  // Item **items;
+} Room;
+
 typedef struct Player
 {
   int xPosition;
@@ -12,11 +22,15 @@ typedef struct Player
 } Player;
 
 int screenSetUp();
-int mapSetUp();
+Room **mapSetUp();
 Player *playerSetUp();
 int handleInput(int input, Player *user);
 int playerMove(int y, int x, Player *user);
 int checkPosition(int newY, int newX, Player *user);
+
+// room functions
+Room * createRoom(int x, int y, int height, int width);
+int drawRoom(Room *room);
 
 int main()
 {
@@ -50,30 +64,62 @@ int screenSetUp()
   return 1;
 }
 
-int mapSetUp()
+Room **mapSetUp()
 {
-  mvprintw(13, 13, "--------");
-  mvprintw(14, 13, "-......-");
-  mvprintw(15, 13, "-......-");
-  mvprintw(16, 13, "-......-");
-  mvprintw(17, 13, "-......-");
-  mvprintw(18, 13, "--------");
+  Room **rooms;
+  rooms = malloc(sizeof(Room)*6);
 
-  mvprintw(2, 40,  "--------");
-  mvprintw(3, 40,  "-......-");
-  mvprintw(4, 40,  "-......-");
-  mvprintw(5, 40,  "-......-");
-  mvprintw(6, 40,  "-......-");
-  mvprintw(7, 40,  "--------");
+  rooms[0] = createRoom(13, 13, 6, 8);
+  drawRoom(rooms[0]);
 
-  mvprintw(10, 40, "------------");
-  mvprintw(11, 40, "-..........-");
-  mvprintw(12, 40, "-..........-");
-  mvprintw(13, 40, "-..........-");
-  mvprintw(14, 40, "-..........-");
-  mvprintw(15, 40, "------------");
+  rooms[1] = createRoom(40, 2, 6, 8);
+  drawRoom(rooms[1]);
 
-  return 0;
+  rooms[2] = createRoom(40, 10, 6, 8);
+  drawRoom(rooms[2]);
+
+  return rooms;
+}
+
+Room * createRoom(int x, int y, int height, int width)
+{
+  Room * newRoom;
+  newRoom = malloc(sizeof(Room));
+  newRoom->xPosition = x;
+  newRoom->yPosition = y;
+  newRoom->height = height;
+  newRoom->width = width;
+
+  return newRoom;
+}
+
+int drawRoom(Room *room)
+{
+  int x;
+  int y;
+
+  // draw top and bottom wall of room
+  for (x = room->xPosition; x < room->xPosition + room->width; x++)
+  {
+    mvprintw(room->yPosition, x, "-");
+    mvprintw(room->yPosition + room->height - 1, x, "-");
+  }
+
+  // draw side walls and floor
+  for (y = room->yPosition + 1; y < room->yPosition + room->height - 1; y++)
+  {
+    // draw side walls for this y value
+    mvprintw(y, room->xPosition, "|");
+    mvprintw(y, room->xPosition + room->width - 1, "|");
+
+    // draw the floor
+    for (x = room->xPosition + 1; x < room->xPosition + room->width - 1; x++)
+    {
+      mvprintw(y, x, ".");
+    }
+  }
+
+  return 1;
 }
 
 Player *playerSetUp()
